38afb7766b66d7d53d26a639160c787ffa026f37
Tristan Verduzco <64928177+VerduzcoTristan@users.noreply.github.com>
2021-09-30 21:42:04 -0500

    format

--- src/main/java/net/corruptmc/nocraftplus/listeners/CraftListener.java
+++ src/main/java/net/corruptmc/nocraftplus/listeners/CraftListener.java
@@ -27,34 +27,34 @@ public class CraftListener implements Listener
     {
         //Check if the current recipe exists
         if (event.getRecipe() != null)
-        { //recipe does not exist
+        {
             InventoryView view = event.getView();
             Player player = (Player) view.getPlayer();
 
             Material material = event.getRecipe().getResult().getType();
             String name = material.name();
 
-                //Check whether or not the current recipe is disabled
-                if (plugin.isBlocked(material))
+            //Check whether or not the current recipe is disabled
+            if (plugin.isBlocked(material))
+            { //recipe is disabled
+                //Check whether or not the player is allowed to use the current recipe
+                if (!player.hasPermission("nocraftplus.bypass." + name.toLowerCase()) && !player.hasPermission("nocraftplus.bypass.*"))
                 {
-                    //Check whether or not the player is allowed to use the current recipe
-                    if (!player.hasPermission("nocraftplus.bypass." + name.toLowerCase()) && !player.hasPermission("nocraftplus.bypass.*"))
-                    {
-                        //Run custom event for API
-                        BlockedCraftingEvent craftEvent = new BlockedCraftingEvent(event);
-                        Bukkit.getServer().getPluginManager().callEvent(craftEvent);
+                    //Run custom event for API
+                    BlockedCraftingEvent craftEvent = new BlockedCraftingEvent(event);
+                    Bukkit.getServer().getPluginManager().callEvent(craftEvent);
 
-                        //Check if an external plugin cancelled the event
-                        if (!craftEvent.isCancelled())
-                        { //event was not cancelled
-                            event.getInventory().setResult(null);
+                    //Check if an external plugin cancelled the event
+                    if (!craftEvent.isCancelled())
+                    {//event was not cancelled
+                        event.getInventory().setResult(null);
 
-                            if (alert)
-                                player.sendMessage(Lang.TITLE.toString() +
-                                        Lang.CRAFTING_DISABLED.toString().replaceAll("%item%", name));
-                        }
+                        if (alert)
+                            player.sendMessage(Lang.TITLE.toString() +
+                                    Lang.CRAFTING_DISABLED.toString().replaceAll("%item%", name));
                     }
                 }
+            }
         }
     }
 
